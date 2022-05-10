1580ce211a2c91f84b840fb7a36a51fa8da89b58
Katerina <ealmazurova@internet.ru>
2022-05-10 14:32:48 +0300

    создана ветка черновик и метод поиска кол-ва эл-тов <= 3

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -4,10 +4,21 @@
 либо задать на старте выполнения алгоритка. 
 При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.*/
-void PrintArray(string[] stringArray)
+void PrintArray(string[] stringArray)               //Метод для печати массива
 {
     for (int i = 0; i < stringArray.Length; i++)
         Console.Write(stringArray[i] + " ");
 }
-string[] firstArray = { "Зима", "крестьянин", "торжествуя", "на", "дровнях", "обновляет", "путь" };
+int FindElement(string[] stringArray)
+{
+    int count = 0;
+    foreach (string s in stringArray)
+    {
+        if (s.Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] firstArray = { "Раз", "Два", "Три", "Четыре", "Пять", "Я", "Иду", "Искать" };  //Наш массив строк
 PrintArray(firstArray);
+Console.WriteLine(FindElement(firstArray));
